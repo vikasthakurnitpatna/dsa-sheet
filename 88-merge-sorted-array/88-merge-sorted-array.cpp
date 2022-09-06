ff531ec361a018c035a5b9b493a23adaaf6b5a38
vikasthakurnitpatna <98396735+vikasthakurnitpatna@users.noreply.github.com>
2022-09-06 12:52:16 +0530

    Time: 8 ms (19.39%), Space: 9.1 MB (30.14%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,27 +1,35 @@
+// #include<bits/stdc++.h>
+
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> v;
-        int i=0;
-        int j=0;
-        int k=0;
-        while(i<m && j<n){
-            if(nums1[i]<nums2[j])
-                 {
-                v.push_back(nums1[i++]);
-                 }
-            else{
-                v.push_back(nums2[j++]);
-                }
-        }
-        while(i<m){
-            v.push_back(nums1[i++]);    
-        }
-        while(j<n){
-            v.push_back(nums2[j++]);
-             
+       int i, k;
+        if(n==0) return;
+  for (i = 0; i < m; i++) {
+    // take first element from arr1 
+    // compare it with first element of second array
+    // if condition match, then swap
+    if (nums1[i] > nums2[0]) {
+      int temp = nums1[i];
+      nums1[i] = nums2[0];
+      nums2[0] = temp;
     }
-        
-        nums1=v;
+
+    // then sort the second array
+    // put the element in its correct position
+    // so that next cycle can swap elements correctly
+    int first = nums2[0];
+    // insertion sort is used here
+    for (k = 1; k < n && nums2[k] < first; k++) {
+      nums2[k - 1] = nums2[k];
+    }
+    nums2[k - 1] = first;
+      }
+         for(int i=0;i<n;i++){
+            nums1.pop_back();
+        }
+        for(int i=0;i<n;i++){
+            nums1.push_back(nums2[i]);
+        }
     }
 };
