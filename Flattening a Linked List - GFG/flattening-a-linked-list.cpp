78361d7bebb95e70b749de7c1bc9c69ba32a8b67
vikasthakurnitpatna <98396735+vikasthakurnitpatna@users.noreply.github.com>
2022-11-05 16:10:31 +0530

    Added solution - LeetHub

---
+++ Flattening a Linked List - GFG/flattening-a-linked-list.cpp
@@ -0,0 +1,158 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
+};
+
+using namespace std;
+
+void printList(Node *Node)
+{
+    while (Node != NULL)
+    {
+        printf("%d ", Node->data);
+        Node = Node->bottom;
+    }
+}
+
+Node* flatten (Node* root);
+
+int main(void) {
+
+	int t;
+	cin>>t;
+	while(t--){
+    int n,m,flag=1,flag1=1;
+    struct Node * temp=NULL;
+	struct Node * head=NULL;
+	struct Node * pre=NULL;
+	struct Node * tempB=NULL;
+	struct Node * preB=NULL;
+		cin>>n;
+        int work[n];
+		for(int i=0;i<n;i++)
+            cin>>work[i];
+		for(int i=0;i<n;i++){
+			m=work[i];
+			--m;
+			int data;
+			scanf("%d",&data);
+			temp = new Node(data);
+			temp->next = NULL;
+			temp->bottom = NULL;
+			
+			if(flag){
+				head = temp;
+				pre = temp;
+				flag = 0;
+				flag1 = 1;
+			}
+			else{
+				pre->next = temp;
+				pre = temp;
+				flag1 = 1;
+			}
+			for(int j=0;j<m;j++){
+				
+				int temp_data;
+				scanf("%d",&temp_data);
+				tempB = new Node(temp_data);
+
+				if(flag1){
+					temp->bottom=tempB;
+					preB=tempB;
+					flag1=0;
+				}
+				else{
+					preB->bottom=tempB;
+					preB=tempB;
+				}
+			}
+		}
+		   Node *fun = head;
+		   Node *fun2=head;
+
+            Node* root = flatten(head);
+            printList(root);
+            cout<<endl;
+
+	}
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Node structure  used in the program
+
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
+};
+*/
+
+/*  Function which returns the  root of 
+    the flattened linked list. */
+// void insertnode(Node*& head, int val)
+// {
+//     Node* n = new Node(val);
+//     if (head == NULL) {
+//         head = n;
+//         return;
+//     }
+  
+//     Node* temp = head;
+//     while (temp->next != NULL) {
+//         temp = temp->next;
+//     }
+//     temp->next = n;
+// }  
+    
+    
+Node *flatten(Node *root)
+{
+   // Your code here
+   vector<int>v;
+   while(root!=NULL){
+       Node* head=root;
+       while(head!=NULL){
+           v.push_back(head->data);
+           head=head->bottom;
+       }
+       root=root->next;
+   }
+   sort(v.begin(),v.end());
+ 
+    Node* h = new Node(v[0]);
+
+   Node* head=h;
+   for(int i=1;i<v.size();i++){
+
+       Node* n = new Node(v[i]);
+
+       head->bottom=n;
+
+       head=head->bottom;
+
+   }
+   return h;
+}
+
